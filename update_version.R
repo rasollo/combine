c7c51bf82c57fd13baead29dc844b40752a68f79
rasollo <rodrigo@netsystems.com.br>
2020-04-20 17:13:58 +0200

    Update and rename update_version to update_version.R

--- update_version.R
+++ update_version.R
@@ -15,17 +15,21 @@ for(yy in 1974:2020){
   for(mm in 1:12){
     
     mms <- sprintf("%02d", mm)
-    for(dd in 1:31){
+    for(dd in 27:31){
     
       # Added a sample timer that going from 1-10 seconds of delay for the next request (TimeOut)
       #x<-sample(1:10,1)
       #Sys.sleep(x)
       #CONFIGURE TIMER FOR MINIMIZING SERVER ERROR PROBLEMS (500)
-      
       # Some months are only 30 days long. this line takes care of that.
       if(!((mm==2||mm==4||mm==6||mm==9||mm==11)&&(dd==31))) {
         # Add - February 28 days # Rodrigo Conde Attanasio - razow@msn.com
-        if(!((mm==2)&&(dd==30)||(mm==2)&&(dd==29)&&(!(yy==72)||(yy==76)||(yy==80)||(yy==84)||(yy==88)||(yy==92)||(yy==96)||(yy==00)||(yy==04)||(yy==08)||(yy==12)||(yy==16)||(yy==20)||(yy==24)||(yy==28))))
+        #mm=2
+        #dd=29
+        #yy
+        if(!((mm==2)&&(dd==30) ||
+             !((mm==2)&&(dd==29)&&((yy==1972)||(yy==1976)||(yy==1980)||(yy==1984)||(yy==1988)||(yy==1992)||(yy==1996)||(yy==2000)||(yy==2004)||(yy==2008)||(yy==2012)||(yy==2016)||(yy==2020)||(yy==2024)||(yy==2028))  
+             )))
         {
           dds <- sprintf("%02d", dd)
           dest <- paste(subDir, yys, mms, dds, ".png", sep="")
@@ -47,7 +51,7 @@ for(yy in 1974:2020){
             message(paste("Downloading ", dest, "...", sep=""))
             download.file(imgsrc, destfile=dest, mode="wb", quiet = T)
             
-            #message(paste('Segundos para o proximo: ',x,sep=''))
+            #message(paste('Next in: ',x,seconds, sep=''))
           
             #rstudio::viewer(url = dest)
           
